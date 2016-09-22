7d3dc93a017268f6422143acbe8b9e4777d4a3d6
mayuria-webonise <mayuri.ardad@weboniselab.com>
2016-09-22 15:15:55 +0530

    PDO assignment using SOLID principles

--- PDO_Databases/index.php
+++ PDO_Databases/index.php
@@ -1,80 +1,137 @@
 <?php
-function sqlPdo($query,$queryType,$databaseType){
-    if(!strcmp($databaseType,'mysql'))
-    {
-        $hostname='localhost';
-        $databaseUsername='root';
-        $databasePassword='mayuri123#';
-        try {
-            $DatabaseDriver = new PDO("mysql:host=$hostname;dbname=e_commerce", $databaseUsername, $databasePassword);
-        }catch (Exception $e){
-            echo $e->getMessage();
-        }
+abstract class Databases{
+    abstract protected function connect();
 
-    }
-    elseif(!strcmp($databaseType,'pgsql'))
+    function selectQuery($query,$databaseDriver)
     {
-        $hostname='localhost';
-        $databaseUsername='postgres';
-        $databasePassword='mayuri123#';
-        try {
-            $DatabaseDriver = new PDO("pgsql:dbname=first;host=$hostname;user=$databaseUsername;password=$databasePassword");
-        }catch (Exception $e){
-            echo $e->getMessage();
-        }
-    }
-    elseif(!strcmp($databaseType,'sqlite')){
-        try{
-            $DatabaseDriver = new PDO("sqlite:".__DIR__."/sqliteDatabase.db");
-        }
-        catch(Exception $e){
-            echo $e->getMessage();
-        }
-
-    }
-    $statementPrepared=$DatabaseDriver->prepare($query);
-    if(!strcmp($queryType,'insert'))
-    {
-        if($statementPrepared->execute()){
-            echo "successful insertion";
-        }
-        else{
-            echo "unsuccessful";
-        }
-    }
-    elseif(!strcmp($queryType,'select'))
-    {
+        $statementPrepared=$databaseDriver->prepare($query);
         $statementPrepared->execute();
         $result = $statementPrepared->fetchAll(PDO::FETCH_CLASS);
-        if(empty($result))
-        {
+        return $result;
+    }
+    function insertQuery($query,$databaseDriver)
+    {
+        $statementPrepared=$databaseDriver->prepare($query);
+        if($statementPrepared->execute()){
+            return true;
+        }
+        else{
             return false;
         }
-        else
-        {
-            echo '<pre>',print_r($result);
+    }
+}
+
+class MysqlDatabase extends Databases{
+    private $user;
+    private $password;
+    function __construct($user,$password){
+        $this->user = $user;
+        $this->password = $password;
+    }
+    function connect()
+    {
+        try {
+            $databaseDriver = new PDO("mysql:host=localhost;dbname=e_commerce", $this->user, $this->password);
+        }catch (Exception $e){
+            echo $e->getMessage();
         }
+        return $databaseDriver;
+    }
+}
+
+class PostgresqlDatabase extends Databases{
+    private $user;
+    private $password;
+    function __construct($user,$password){
+        $this->user = $user;
+        $this->password = $password;
+    }
+    function connect()
+    {
+        try {
+            $databaseDriver = new PDO("pgsql:dbname=first;host=localhost", $this->user, $this->password);
+        }catch (Exception $e){
+            echo $e->getMessage();
+        }
+        return $databaseDriver;
+
+    }
+}
+
+class SqliteDatabase extends Databases{
+    private $user;
+    private $password;
+    function __construct($user,$password){
+        $this->user = $user;
+        $this->password = $password;
+    }
+    function connect()
+    {
+        try {
+            $databaseDriver = new PDO("sqlite:".__DIR__."/sqliteDatabase.db", $this->user, $this->password);
+        }catch (Exception $e){
+            echo $e->getMessage();
+        }
+        return $databaseDriver;
+
     }
 }
 
 if(isset($_POST['mysql']))
 {
-    sqlPdo( "INSERT INTO users (idusers, user_name, email,password)
-VALUES (9,'Prajakta','prajkta@gmail.com','mayuri')","insert",'mysql');
-    sqlpdo('select * from users','select','mysql');
+    $mysqlDatabase = new MysqlDatabase('root','mayuri123#');
+    $databaseDriver = $mysqlDatabase->connect();
+    if($databaseDriver){
+        echo "connection Successful<br>";
+    }else{
+        echo "connection failed<br>";
+    }
+    $status = $mysqlDatabase->insertQuery("INSERT INTO users (idusers, user_name, email,password) VALUES (10,'Prajakta','prajkta@gmail.com','mayuri')",$databaseDriver);
+    if($status){
+        echo "insertion success<br>";
+    }else{
+        echo "insertion failed<br>";
+    }
+    $result = $mysqlDatabase->selectQuery("select * from users",$databaseDriver);
+    echo '<pre>',print_r($result);
 }
 if(isset($_POST['postgresql']))
 {
-    sqlPdo("INSERT INTO company (id, name,age,address,salary)
-    VALUES (2,'Prajakta',22,'Pune',50000)",'insert','pgsql');
-    sqlpdo('select * from company','select','pgsql');
+    $postgresqlDatabase = new PostgresqlDatabase('postgres','mayuri123#');
+    $databaseDriver = $postgresqlDatabase->connect();
+    if($databaseDriver){
+        echo "connection Successful<br>";
+    }else{
+        echo "connection failed<br>";
+    }
+    $status = $postgresqlDatabase->insertQuery("INSERT INTO company (id, name,age,address,salary) VALUES (5,'Prajakta',22,'Pune',50000)",$databaseDriver);
+    if($status){
+        echo "insertion success<br>";
+    }else{
+        echo "insertion failed<br>";
+    }
+    $result = $postgresqlDatabase->selectQuery('select * from company',$databaseDriver);
+    echo '<pre>',print_r($result);
 }
 
 if(isset($_POST['sqlite']))
 {
-    sqlPdo("INSERT INTO users (id,user_name,age,address,salary)
-    VALUES (2,'Prajakta',22,'Pune',50000);",'insert','sqlite');
-    sqlpdo('select * from users','select','sqlite');
+    $sqliteDatabase = new SqliteDatabase('','');
+    $databaseDriver = $sqliteDatabase->connect();
+    if($databaseDriver){
+        echo "connection Successful<br>";
+    }else{
+        echo "connection failed<br>";
+    }
+    $status = $sqliteDatabase->insertQuery("INSERT INTO users (id,user_name,age,address,salary)
+    VALUES (1,'Prajakta',22,'Pune',50000);",$databaseDriver);
+    if($status){
+        echo "insertion success<br>";
+    }else{
+        echo "insertion failed<br>";
+    }
+    $result = $sqliteDatabase->selectQuery('select * from users;',$databaseDriver);
+    echo '<pre>',print_r($result);
 }
 ?>
 <html>
